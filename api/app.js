c5ac3616614fda969f73226e6a94f2f7881f18f7
Awadesh <36awadesh@gmail.com>
2024-07-27 20:53:29 +0530

    chore(deployment): prepare for deployment
    - Configured environment variables and settings for deployment.
    - Updated documentation to reflect the new deployment process.
    - Performed necessary code adjustments to ensure compatibility.
    
    These changes set up the project for a successful deployment, ensuring all necessary configurations and adjustments are in place.

--- api/app.js
+++ api/app.js
@@ -26,8 +26,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running!`);
 });
 
 app.use('/api/user', userRoutes);
